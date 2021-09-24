88b86bcb2e40c7e7d03cad555929ee5b7cab80fe
Michael Chinweike <infinity_michael@yahoo.com>
2021-09-24 07:01:29 +0100

    fix: refactored connect demo to use vanilla js #1

--- examples/connect/src/connectVault.js
+++ examples/connect/src/connectVault.js
@@ -1,12 +1,5 @@
-/* eslint-disable */
-
 import { Network } from "@verida/client-ts";
 import { VaultAccount } from "@verida/account-web-vault";
-import {
-  ErrorMessages,
-  ProfileDetails,
-  ProfileDocument,
-} from "@/interface/interface";
 
 const EventEmitter = require("events");
 
@@ -18,30 +11,27 @@ const CONTEXT_NAME = "Verida: Connect Demo";
 const VERIDA_TESTNET_DEFAULT_SERVER = "https://db.testnet.verida.io:5002/";
 
 class ConnectVault extends EventEmitter {
-  private connection: any;
-  private profileInstance: any;
-  private account: any;
-  public did?: string;
+  connection = {};
+  profileInstance = {};
+  account = null;
+  did = "";
   error = {};
   profile = {};
 
   /**
    * Public method for initializing this app
    */
-  async initApp(): Promise<void> {
+  async initApp() {
     if (!this.connection) {
       await this.connect();
     }
   }
 
-  appInitialized(): boolean {
+  appInitialized() {
     return this.connection !== null;
   }
 
-  /**
-   * Private method for connecting to the vault
-   */
-  public async connect(): Promise<void> {
+  async connect() {
     this.account = new VaultAccount({
       defaultDatabaseServer: {
         type: "VeridaDatabase",
@@ -54,7 +44,7 @@ class ConnectVault extends EventEmitter {
       vaultConfig: {
         logoUrl: LOGO_URL,
       },
-    } as unknown as any);
+    });
 
     this.connection = await Network.connect({
       client: {
@@ -72,7 +62,7 @@ class ConnectVault extends EventEmitter {
     this.emit("initialized");
   }
 
-  private async initProfile(): Promise<void> {
+  async initProfile() {
     const services = this;
     const client = this.connection.getClient();
     this.profileInstance = await client.openPublicProfile(
@@ -80,15 +70,12 @@ class ConnectVault extends EventEmitter {
       "Verida: Vault"
     );
 
-    const cb = async function (): Promise<void> {
+    const cb = async function () {
       const data = await services.profileInstance.getMany();
-      services.profile = data.reduce(
-        (result: any, item: ProfileDocument): ProfileDetails => {
-          result[item.key] = item.value;
-          return result;
-        },
-        {}
-      );
+      services.profile = data.reduce((result, item) => {
+        result[item.key] = item.value;
+        return result;
+      }, {});
 
       services.emit("profileChanged", services.profile);
     };
@@ -97,21 +84,18 @@ class ConnectVault extends EventEmitter {
     await cb();
   }
 
-  handleErrors(error: ErrorMessages) {
+  handleErrors(error) {
     this.error = error;
     this.emit("error", error);
   }
 
-  async logout(): Promise<void> {
+  async logout() {
     await this.account.disconnect();
     this.connection = null;
-    this.dataStore = null;
-    this.currentNote = null;
     this.profileInstance = null;
     this.account = null;
     this.did = "";
     this.error = {};
-    this.notes = [];
     this.profile = {};
   }
 }
--- examples/connect/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/scss/app.scss";
-
-createApp(App).mount("#app");
